e2d3eb4a315b851f637ca707a3258916b95b784c
Deepak <nagadeepak000@gmail.com>
2025-05-28 17:49:23 +0530

    added args support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use sha256::hash;
-
+use std::{env, process::exit};
 fn main() {
-    let input = "abcd";
-    println!("{}",hash(input));
+    let input: Vec<_> = env::args().collect();
+    if input.len() != 2 {
+        println!("Expected only one argument - str to be hashed");
+        exit(1);
+    }
+    println!("{}", hash(&input[1]));
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -18,6 +21,9 @@ mod tests {
 
     #[test]
     fn test_abcd() {
-        assert_eq!(hash("abcd"), "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589");
+        assert_eq!(
+            hash("abcd"),
+            "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589"
+        );
     }
 }
